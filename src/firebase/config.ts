402e0a0fb9ae9b3a631f2a2d0ddd5186f3d6ab56
Boost Engine <boostengine001@gmail.com>
2025-11-01 19:16:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9151577106-73f87",
+  "appId": "1:41474245921:web:2627126e02ea9c9ea6aa60",
+  "apiKey": "AIzaSyC9Hn7dSEYsLH1YXlmn-U9x87-6DQ_GTG8",
+  "authDomain": "studio-9151577106-73f87.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "41474245921"
+};
